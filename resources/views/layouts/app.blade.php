79714dbf8ed29c1f736b6e91d95cc71d76f107f7
deca-francesco <francescodecarlo70@gmail.com>
2025-03-17 15:07:02 +0100

    UPDATE: logo and favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link rel="icon" type="image/x-icon" href="{{ Vite::asset('resources/assets/img/logo/favicon.ico') }}">
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
