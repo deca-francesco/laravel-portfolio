c3a744b0b13236fa2dd1b71e66bc82bc51440ba6
deca-francesco <francescodecarlo70@gmail.com>
2025-03-19 18:27:49 +0100

    ADD: comments to pivot table project_technology seeder

--- database/seeders/ProjectTechnologyTableSeeder.php
+++ database/seeders/ProjectTechnologyTableSeeder.php
@@ -31,11 +31,13 @@ class ProjectTechnologyTableSeeder extends Seeder
 
             $project->technologies()->attach(
                 // for($i = 0; $i < rand(0, 3); $i++) {
-                //     $technologiesIdArray[rand(0, (count($idArray) - 1))]
+                //     $technologiesIdArray[rand(0, (count($idArray) - 1))];
                 // }
 
-                // random è un metodo delle collection
-                // pluck prende i valori delle chiavi id dalla collection
+                // random prende una o più istanze random dalla collection
+                // rand dice quante prenderne
+                // pluck prende solo gli id dalle istanze
+                // toArray perché attach vuole un array di id
                 $technologiesCollection->random(rand(0, 3))->pluck('id')->toArray()
             );
         }
